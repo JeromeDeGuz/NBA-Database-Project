261bae1dcc8cb0aa007212e98bea343bf39794da
gurwinderkhandal41 <gurwinderkhandal@gmail.com>
2025-12-02 10:10:39 -0600

    d

--- Stage6/project.java
+++ Stage6/project.java
@@ -9,8 +9,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.util.Scanner;
-//gggggggggggg
-//test
+
 public class project {
     static Connection connection;
     public static void main(String[] args) {
