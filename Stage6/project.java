14328cd087bda1a6a3f6a1d5e40d25f05e447023
HerrerAaron8 <herrera8@myumanitoba.ca>
2025-12-03 16:30:27 -0600

    delete uranium

--- Stage6/project.java
+++ Stage6/project.java
@@ -41,40 +41,8 @@ public class project {
         System.out.println("Exiting...");
     }
 
-    // handles connecting to uranium
-    public static void uraniumconnect() {
-        Properties prop = new Properties();
-        String fileName = "auth.cfg";
-        try {
-            FileInputStream configFile = new FileInputStream(fileName);
-            prop.load(configFile);
-            configFile.close();
-        } catch (FileNotFoundException ex) {
-            System.out.println("Could not find config file.");
-            System.exit(1);
-        } catch (IOException ex) {
-            System.out.println("Error reading config file.");
-            System.exit(1);
-        }
-        String username = (prop.getProperty("username"));
-        String password = (prop.getProperty("password"));
-
-        if (username == null || password == null) {
-            System.out.println("Username or password not provided.");
-            System.exit(1);
-        }
-
-        String connectionUrl = "jdbc:sqlserver://uranium.cs.umanitoba.ca:1433;"
-                + "database=cs3380;"
-                + "user=" + username + ";"
-                + "password=" + password + ";"
-                + "encrypt=false;"
-                + "trustServerCertificate=false;"
-                + "loginTimeout=30;";
-    }
-
     // the general console loop
-    // the general console loop
+   // the general console loop
     public static void runConsole(MyDatabase db) {
 
         Scanner console = new Scanner(System.in);
